23eec3176bd03133c2b9266e79b33cc5ccb968d6
Gtosta96 <gabrieltosta3@gmail.com>
2016-03-16 17:07:10 -0300

    evoluido ngmessages

--- www/js/directives/directives.js
+++ www/js/directives/directives.js
@@ -1,8 +1,18 @@
 var app = angular.module('pccApp.directives', []);
 
+app.directive("errorTemplates", function(){
+	return {
+		restrict: "E",
+		templateUrl: "/templates/util/directives/errorTemplates.html"
+	}
+})
+
 app.directive("errorMessage", function(){
 	return {
 		restrict: "A",
+		scope: {
+			'field': '@'
+		},
 		templateUrl: "/templates/util/directives/errorMessage.html"
 	}
-})
+});
