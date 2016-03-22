ceff5fe539807ca0fd0a9cbeaac8a250ff23f719
Gtosta96 <gabrieltosta3@gmail.com>
2016-03-22 01:03:21 -0300

    iniciado diretiva compareTo

--- www/js/directives/directives.js
+++ www/js/directives/directives.js
@@ -11,11 +11,29 @@ app.directive("displayMessage", function(){
 	return {
 		restrict: "E",
 		scope: {
-			'form': '=',
-			'attribute': '@',
-			'display': '@',
-			'message': '@'
+			form: '=',
+			attribute: '@',
+			display: '@',
+			message: '@'
 		},
 		templateUrl: "/templates/util/directives/displayMessage.html"
 	}
+});
+
+app.directive("compareTo", function() {
+    return {
+        require: "ngModel",
+        scope: {
+            "otherModelValue": "=compareTo"
+        },
+        link: function(scope, element, attributes, ngModel) {
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
 });
