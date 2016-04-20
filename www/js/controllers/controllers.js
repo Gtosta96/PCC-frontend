1e2113a675d440c8a86aedd82b869a653e2b0e85
Gtosta96 <gabrieltosta3@gmail.com>
2016-04-20 16:39:45 -0300

    atualiazado

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -7,5 +7,28 @@ function go ($state, tela) {
 };
 
 function callbackError(error) {
-	console.log("Error: " + JSON.stringify(error));
+	console.log("Ocorreu um erro: %o", error);
 };
+
+ngFB.login({scope: 'email'}).then(
+        function (response) {
+            if (response.status === 'connected') {
+                ngFB.api({
+                    path: '/me',
+                    params: {fields: 'id, first_name, last_name, email, picture'}
+                }).then(
+                    function (user) {
+                        console.log(user);
+                        $scope.user.email = user.email;
+                        $scope.user.first_name = user.first_name;
+                        $scope.user.last_name = user.last_name;
+                        $scope.user.id_facebook = user.id;
+                        $scope.user.photo_facebook = user.picture.data.url;
+                    },
+                    function (error) {
+                        alert('Facebook error: ' + error.error_description);
+                });
+            } else {
+                alert('Facebook login failed');
+            }
+        });
