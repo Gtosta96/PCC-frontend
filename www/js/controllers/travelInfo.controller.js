de2468423a616f54b18ceb4885bc75b04791f2c1
Gtosta96 <gabrieltosta3@gmail.com>
2016-06-16 23:25:46 -0300

    evoluido travelInfo

--- www/js/controllers/travelInfo.controller.js
+++ www/js/controllers/travelInfo.controller.js
@@ -7,15 +7,26 @@
   ]);
 
   app.controller('TravelInfoCtrl', function($stateParams, $scope, TravelInfoRestService, HandlerService) {
-
-    var vm = this;
-
     $scope.$on('$ionicView.beforeEnter', function(event, viewData) {
       viewData.enableBack = true;
     });
 
+    var vm = this;
     TravelInfoRestService.get($stateParams).$promise.then(function(result) {
       vm.travelInfo = result;
+			vm.setRankCallback = loadRanking(vm.travelInfo.rank);
     }, HandlerService.callbackError);
+
+		function loadRanking(rank) {
+			return {
+				iconOn: 'ion-ios-star',
+				iconOff: 'ion-ios-star-outline',
+				iconOnColor: '#FFC900',
+				iconOffColor:  '#DDD',
+				rating:  rank,
+				readOnly: true,
+				callback: function() {}
+			}
+		}
   });
 }());
